553fcc73f55502ffd546bf5320fa17b13c58ce04
JerryJox <profesionalmail@msn.com>
2023-03-24 20:07:27 +0000

    upload files

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My granma", "His turtle", "My bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function generateExcuse() {
+    let randomWhoIndex = Math.floor(Math.random() * who.length);
+    let randomWhatIndex = Math.floor(Math.random() * what.length);
+    let randomWhenIndex = Math.floor(Math.random() * when.length);
+
+    return (
+      who[randomWhoIndex] +
+      " " +
+      what[randomWhatIndex] +
+      " " +
+      when[randomWhenIndex]
+    );
+  }
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
